b0ed85091b5be7cc4e9a716b854202acc2f5e963
Anika Torres Davalos <atorresda2023@gmail.com>
2026-04-29 21:09:40 +0100

    Starting. Zero info, just files being created

---
+++ Functions.c
@@ -0,0 +1,5 @@
+//
+// Created by Anika Torres Davalos on 4/29/2026.
+//
+
+#include "Functions.h"
---
+++ Functions.h
@@ -0,0 +1,8 @@
+//
+// Created by Anika Torres Davalos on 4/29/2026.
+//
+
+#ifndef AC_WAVEFORMS_FUNCTIONS_H
+#define AC_WAVEFORMS_FUNCTIONS_H
+
+#endif //AC_WAVEFORMS_FUNCTIONS_H
