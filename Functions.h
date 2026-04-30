3f1a832416de847519eb30b55e261682bc82d669
Anika Torres Davalos <atorresda2023@gmail.com>
2026-04-30 19:02:09 +0100

    rand

--- Functions.h
+++ Functions.h
@@ -5,4 +5,21 @@
 #ifndef AC_WAVEFORMS_FUNCTIONS_H
 #define AC_WAVEFORMS_FUNCTIONS_H
 
+typedef struct{
+    float timeStamp; // every 0.0002 s
+    float phase_A_Vol; //
+    float phase_B_Vol;
+    float phase_C_Vol;
+    float line_current;
+    float frequency;
+    float power_factor;
+    float thd_percent;
+}WaveformSample;
+
+
+
+
+
+
+
 #endif //AC_WAVEFORMS_FUNCTIONS_H
