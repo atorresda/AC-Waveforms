123bb77c8dfa3411aa05992c497b20cf27c0026d
Anika Torres Davalos <atorresda2023@gmail.com>
2026-05-08 22:56:28 +0100

    NEW STEP. malloc() - use malloc in main with new rows added to the data_try - Made sure to include a safety net in case malloc fails

--- io.c
+++ io.c
@@ -16,7 +16,7 @@ void load_csv(WaveformSample *array){
     fgets(line, sizeof(line), fp); // skip header
 
     int i = 0;
-    while (fgets(line, sizeof(line), fp) !=NULL && i<1){
+    while (fgets(line, sizeof(line), fp) !=NULL && i<29){
         sscanf(line,"%f,%f,%f,%f,%f,%f,%f,%f",
                &array[i].timeStamp,
                &array[i].phase_A_Vol,
@@ -37,13 +37,13 @@ void load_csv(WaveformSample *array){
     }else {
         fprintf(output_fp, "Values: \n Time: %f\n Phase A: %f\n Phase B: %f\n Phase C: %f\n Current: %f\n Frequency: %f\n Power Fac: %f\n THD percent: %f\n",
                 array[0].timeStamp,
-                array[0].phase_A_Vol,
-                array[0].phase_B_Vol,
-                array[0].phase_C_Vol,
-                array[0].line_current,
-                array[0].frequency,
-                array[0].power_factor,
-                array[0].thd_percent);
+                array[1].phase_A_Vol,
+                array[2].phase_B_Vol,
+                array[3].phase_C_Vol,
+                array[4].line_current,
+                array[5].frequency,
+                array[6].power_factor,
+                array[28].thd_percent);
         fclose(output_fp);
     }
 
--- main.c
+++ main.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
+#include<stdlib.h>
 #include "waveform.h"
 #include "io.h"
 
 int main() {
-    WaveformSample array[2];
+
+    int rows_number = 28;
+    WaveformSample *array;
+
+    array = (WaveformSample *)malloc(rows_number * sizeof(WaveformSample));
+
+    if (array == NULL) {
+        printf("Memory allocation failed!\n");
+        return 1;
+    }
+
     load_csv(array);
+
+    free(array);
 
     return 0 ;
 }
