05f3a600c58d11145c9ae655db211e28663b948f
Anika Torres Davalos <atorresda2023@gmail.com>
2026-05-08 20:49:21 +0100

    - Follow class slides to open the file and print it out in an external txt file

--- io.c
+++ io.c
@@ -6,8 +6,29 @@ void load_csv(WaveformSample *array){
 
     fp = fopen("data/data_try.csv","r");
 
+    if (fp == NULL){
+        printf("Error: Cannot open file\n");
+        return 1;
+    }
+
+    char line[256];
+
+    fgets(line, sizeof(line), fp); // skip header
+
+    while (fgets(line, sizeof(line), fp) !=NULL){
+        sscanf(fp,"%f %f %f", array[0]);
+    }
+
+    fopen("outcome/report.txt", "w");
+    fprintf("Values: \n Time: %f\n Phase A: %f\n Phase B: %f\n",
+            array[0].timeStamp,
+            array[0].phase_A_Vol,
+            array[0].phase_B_Vol)'];
+    fclose(report.txt);
+
     fclose(fp);
 
+    return 0;
 }
 
 
