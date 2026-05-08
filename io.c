172130e3d4d9cb2cb1d4182e563ca787bdab8293
Anika Torres Davalos <atorresda2023@gmail.com>
2026-05-08 21:44:22 +0100

    - I confused sscanf with fprintf     - Switched the &'s     - Also, add the remaining variables from the structure

--- io.c
+++ io.c
@@ -16,18 +16,26 @@ void load_csv(WaveformSample *array){
     fgets(line, sizeof(line), fp); // skip header
 
     while (fgets(line, sizeof(line), fp) !=NULL){
-        sscanf(fp,"%f %f %f %f %f %f %f", array[0]);
+        sscanf(fp,"%f %f %f %f %f %f %f",
+               &array[0].timeStamp,
+               &array[0].phase_A_Vol,
+               &array[0].phase_B_Vol,
+               &array[0].phase_C_Vol,
+               &array[0].line_current,
+               &array[0].frequency,
+               &array[0].power_factor,
+               &array[0].thd_percent);
     }
 
     FILE *output_fp = fopen("outcome/report.txt", "w");
     fprintf("Values: \n Time: %f\n Phase A: %f\n Phase B: %f\n",
-            &array[0].timeStamp,
-            &array[0].phase_A_Vol,
-            &array[0].phase_B_Vol,
-            &array[0].line_current,
-            &array[0].frequency,
-            &array[0].power_factor,
-            &array[0].thd_percent);
+            array[0].timeStamp,
+            array[0].phase_A_Vol,
+            array[0].phase_B_Vol,
+            array[0].line_current,
+            array[0].frequency,
+            array[0].power_factor,
+            array[0].thd_percent);
     fclose(output_fp);
 
     fclose(fp);
