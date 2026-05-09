cc072995aa573f34206a79e45564e2cc6cd7b0d0
Anika Torres Davalos <atorresda2023@gmail.com>
2026-05-09 18:54:59 +0100

    - change double to float

--- io.c
+++ io.c
@@ -17,7 +17,7 @@ void load_csv(WaveformSample *array){
 
     int i = 0;
     while (fgets(line, sizeof(line), fp) !=NULL && i<1002){
-        sscanf(line,"%f,%f,%f,%f,%f,%f,%f,%f",
+        sscanf(line,"%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf",
                &array[i].timeStamp,
                &array[i].phase_A_Vol,
                &array[i].phase_B_Vol,
@@ -35,12 +35,12 @@ void load_csv(WaveformSample *array){
         printf("Error:Couldn't create report.txt\n");
 
     }else {
-        fprintf(output_fp, "Values: \n Time: %f\n Phase A: %f\n Phase B: %f\n Phase C: %f\n Current: %f\n Frequency: %f\n Power Fac: %f\n THD percent: %f\n",
+        fprintf(output_fp, "Values: \n Time: %lf\n Phase A: %lf\n Phase B: %lf\n Phase C: %lf\n Current: %lf\n Frequency: %lf\n Power Fac: %lf\n THD percent: %lf\n",
                 array[0].timeStamp,
                 array[3].phase_A_Vol,
                 array[50].phase_B_Vol,
                 array[490].phase_C_Vol,
-                array[600].line_current,
+                array[598].line_current,
                 array[800].frequency,
                 array[801].power_factor,
                 array[999].thd_percent);
--- waveform.h
+++ waveform.h
@@ -11,14 +11,14 @@
 // RMS over one full cycle = 100 samples
 
 typedef struct{
-    float timeStamp; // every 0.0002 s
-    float phase_A_Vol; //325.27 V peak = 230 V RMS * sqrt(2)
-    float phase_B_Vol;
-    float phase_C_Vol;
-    float line_current; // Star connection. Tolerance, typical 3.5 A
-    float frequency; // Cycles per second 50 Hz (10 complete cycles) (1 in 0.02 s)
-    float power_factor;// Ratio real / apparent. 1.0 is ideal (0.95 typical)
-    float thd_percent;//noise. 8% limit
+    double timeStamp; // every 0.0002 s
+    double phase_A_Vol; //325.27 V peak = 230 V RMS * sqrt(2)
+    double phase_B_Vol;
+    double phase_C_Vol;
+    double line_current; // Star connection. Tolerance, typical 3.5 A
+    double frequency; // Cycles per second 50 Hz (10 complete cycles) (1 in 0.02 s)
+    double power_factor;// Ratio real / apparent. 1.0 is ideal (0.95 typical)
+    double thd_percent;//noise. 8% limit
 }WaveformSample;
 
 #endif //AC_WAVEFORMS_WAVEFORM_H
